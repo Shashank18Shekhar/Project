0c708e290b7ce3e0f19149f46b273dec7be2a05a
Shashank18Shekhar <sshashankshekhar1998@gmail.com>
2020-07-04 16:53:50 +0530

    Updates

---
+++ precache-manifest.e250b1931aef1a76e75bd91dc894dd34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c049eebf27a95a11f804",
+    "url": "/Project/static/css/main.ba9bdaf9.chunk.css"
+  },
+  {
+    "revision": "c049eebf27a95a11f804",
+    "url": "/Project/static/js/main.6298d000.chunk.js"
+  },
+  {
+    "revision": "bf70e2e0dab4b2bc1080",
+    "url": "/Project/static/js/runtime~main.e6490b13.js"
+  },
+  {
+    "revision": "706930dc4e23dc596697",
+    "url": "/Project/static/css/2.f262f84e.chunk.css"
+  },
+  {
+    "revision": "706930dc4e23dc596697",
+    "url": "/Project/static/js/2.0113bde6.chunk.js"
+  },
+  {
+    "revision": "9241ca7fe54c78b4307a5859a276cd2c",
+    "url": "/Project/index.html"
+  }
+];
